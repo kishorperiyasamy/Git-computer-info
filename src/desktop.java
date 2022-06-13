1ac4184e8a37047e67a9ab779f06cdff62da698f
kishorperiyasamy <periyasamykishor@gmail.com>
2022-06-13 23:09:45 +0530

    some computer info

---
+++ src/desktop.java
@@ -0,0 +1,20 @@
+
+public class desktop {
+	
+	private void desktopsize() {
+		System.out.println("desktopsize is 17inch");
+		
+		
+	}
+	private void desktopcolour() {
+		System.out.println("colour is black");
+		
+
+	}
+	public static void main(String[] args) {
+		desktop x=new desktop();
+		x.desktopcolour();
+		x.desktopsize();
+	}
+
+}
